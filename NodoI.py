dd7a05a8b0a577462a2b2d3040e5d179b7d4788a
Rail Neira <r.neira03@ufromail.cl>
2023-09-23 20:46:27 -0300

    Agrega implementación de algoritmo de generación de números aleatorios trilium

--- NodoI.py
+++ NodoI.py
@@ -5,20 +5,48 @@ import random, asyncio
 n = 3
 
 class NodoI(Multiparte.Nodo, threading.Thread):
-    def __init__(self, id, key):
+    def __init__(self, id):
+        # Variables para la suma multiparte
         self.id = id
-        self.key = key
+        self.key = None
+        self.partes = []
+        self.otrasPartes = []
+        self.sumaParcial = None
+        self.sumasParciales = []
+        self.sumaTotal = None
+
+        self.nroPartesEnviadas = 0
+
+        #Variables para el trilium
+        self.vector = self.generarVector(id)
+        self.intermedio1 = [66,69,66][id]
+        self.intermedio2 = [69,78,87][id]
 
         threading.Thread.__init__(self)
         self.__threadCondition = threading.Condition()
         
-        self.partes = self.separarKey()
-        self.otrasPartes = []
-        self.sumasParciales = []
+    # Metodos trilium
+    def generarVector(self, id, seed=0):
+        # TODO incorporar el uso de la seed
+        largo = [93,84,111][id]
 
-        self.nroPartesEnviadas = 0
-
+        return [i % 2 for i in range(largo)]
+    
+    def primerXor(self):
+        return (self.vector[-3] & self.vector[-2]) ^ self.vector[-1] ^ self.vector[self.intermedio1]
+    
+    def moverVector(self, St, s):
+          nuevo = St ^ s ^ self.vector[self.intermedio2]
+          self.vector.insert(0, nuevo)
+          self.vector.pop()
+        
+    
+    # Metodos suma multiparte
     def getMyPart(self, mensaje, current=None):
+        #TODO: quitar self.nroPartesEnviadas y usar otra estrategia
+        if len(self.partes) < n:
+            raise Multiparte.NotReadyError()
+
         if self.nroPartesEnviadas == 3:
             return 0
         
@@ -28,14 +56,14 @@ class NodoI(Multiparte.Nodo, threading.Thread):
         return tuParte
     
     def getPartialSum(self, current=None):
-        if len(self.otrasPartes) < n:
+        if self.sumaParcial == None:
             raise Multiparte.NotReadyError()
         return sum(self.otrasPartes)
     
     def getFinalSum(self, current=None):
-        if len(self.sumasParciales) < n:
+        if self.sumaTotal == None:
             raise Multiparte.NotReadyError()
-        return sum(self.sumasParciales)
+        return self.sumaTotal
 
     def separarKey(self):
         partes = []
@@ -47,8 +75,6 @@ class NodoI(Multiparte.Nodo, threading.Thread):
     ## Hilo de ejecución para las operaciones de cliente.
     def run(self):
         with self.__threadCondition:
-            print("Mi número secreto es {}".format(self.key))
-            print("Las partes que voy a compartir son {}".format(self.partes))
             proxies = []
             for i in range(n):
                 if i != int(self.id): 
@@ -65,46 +91,91 @@ class NodoI(Multiparte.Nodo, threading.Thread):
                             conectado = True
                             print("Conectado al nodo {} :D".format(i))
                         except:
-                            self.__threadCondition.wait(3)            
+                            self.__threadCondition.wait(3)     
+            print("---------------Conexión lista--------------------\n\n")
+            while True:
+                self.nroPartesEnviadas = 0
+                self.key = self.primerXor()
+                self.partes = self.separarKey()
+                print("Mi número secreto (s) es {}".format(self.key))
+                print("Las partes que voy a compartir son {}".format(self.partes))
+                print("Mi vector tiene largo: {}".format(len(self.vector)))
 
-            # Pide todas las partes incluyendo la propia.
-            self.otrasPartes.append(self.getMyPart(""))
-            for i, proxy in enumerate(proxies):
-                parteRecibida = False
-                while not parteRecibida:
-                    try:
-                        print("Necesito la parte de {}".format(i), end=" ")
-                        parte = proxy.getMyPart("")
-                        self.otrasPartes.append(parte)
-                        print("que es {}".format(parte))
-                    except Multiparte.NotReadyError:
-                        self.__threadCondition.wait(1)
-                        print("")
-                    else:
-                        parteRecibida = True
-            print("Las partes que he recolectado son: {}".format(self.otrasPartes), end=" ")
-            print("Y suman {}".format(sum(self.otrasPartes)))
 
-            # Suma las partes de otros y genera su suma parcial
-            self.sumasParciales.append(sum(self.otrasPartes))
+                # Pide todas las partes incluyendo la propia.
+                self.otrasPartes = []
+                self.otrasPartes.append(self.getMyPart(""))
+                for i, proxy in enumerate(proxies):
+                    parteRecibida = False
+                    numeroDelNodo = proxy.ice_toString().split(" ")[0].split("_")[1]
+                    while not parteRecibida:
+                        try:
+                            print("Necesito la parte de {}".format(numeroDelNodo), end=" ")
+                            parte = proxy.getMyPart("")
+                            self.otrasPartes.append(parte)
+                            print("que es {}".format(parte))
+                        except:
+                            self.__threadCondition.wait(1)
+                            print("")
+                        else:
+                            parteRecibida = True
 
-            # Solcita las sumas parciales de los otros
-            for i, proxy in enumerate(proxies):
-                sumaRecibida = False
-                while not sumaRecibida:
-                    try:
-                        print("Necesito la suma parcial de {}".format(i), end=" ")
-                        suma = proxy.getPartialSum()
-                        self.sumasParciales.append(suma)
-                        print("que es {}".format(suma))
-                    except:
-                        self.__threadCondition.wait(1)
-                        print("")
-                    else:
-                        sumaRecibida = True
-            print("Las sumas que he recolectado son: {}".format(self.sumasParciales))
-            print("La suma final es: {}".format(sum(self.sumasParciales)))
-        # ic.waitForShutdown()
+                self.sumaTotal = None # Importante borrar para no enviar por accidente
+                self.sumaParcial = sum(self.otrasPartes)
+                print("Las partes que he recolectado son: {}".format(self.otrasPartes), end=" ")
+                print("Y suman {}".format(self.sumaParcial))
+
+                # Suma las partes de otros y genera su suma parcial
+                self.sumasParciales = []
+                self.sumasParciales.append(self.sumaParcial)
+
+                # Solcita las sumas parciales de los otros
+                for i, proxy in enumerate(proxies):
+                    sumaRecibida = False
+                    numeroDelNodo = proxy.ice_toString().split(" ")[0].split("_")[1]
+                    while not sumaRecibida:
+                        try:
+                            print("Necesito la suma parcial de {}".format(numeroDelNodo), end=" ")
+                            suma = proxy.getPartialSum()
+                            self.sumasParciales.append(suma)
+                            print("que es {}".format(suma))
+                        except:
+                            self.__threadCondition.wait(1)
+                            print("")
+                        else:
+                            sumaRecibida = True
+                self.partes = [] # Es importante borrar partes para que en la siguiente iteración no se envíe una parte anterior por accidente
+                self.sumaTotal = sum(self.sumasParciales)
+                print("Las sumas que he recolectado son: {}".format(self.sumasParciales))
+                print("La suma final es: {}".format(self.sumaTotal))
+                
+                sumasTotales = []
+                for i, proxy in enumerate(proxies):
+                    sumaTotalRecibida = False
+                    numeroDelNodo = proxy.ice_toString().split(" ")[0].split("_")[1]
+                    while not sumaTotalRecibida:
+                        try:
+                            print("Necesito la suma final de {}".format(numeroDelNodo), end=" ")
+                            total = proxy.getFinalSum()
+                            sumasTotales.append(total)
+                            print("que es {}".format(total))
+                        except:
+                            self.__threadCondition.wait(1)
+                            print("")
+                        else:
+                            sumaTotalRecibida = True
+
+                self.sumaParcial = None # Es importante borrar sumaParcial para que no se envíe por accidente en la siguiente iteración de otro nodo
+                assert all(x == sumasTotales[0] for x in sumasTotales) # Si una de las sumas totales no coincide se detiene la ejecución
+                
+                St = sum(self.sumasParciales) % 2
+                print("St es: {}".format(St))
+                self.moverVector(St, self.key)
+
+
+                input("Presiona Enter para continuar...")
+                #TODO: Como evitar que se llamen a los datos de la iteración pasada
+            # ic.waitForShutdown()
 
 # CODIGO DEL SERVER ---------------------------------------------------------------------------------------------
 # Está en el mismo archivo para ponder utilizar el objeto ic = Ice.initialize()
@@ -113,7 +184,6 @@ import sys, traceback, Ice
 
 
 id = int(sys.argv[1])
-key = int(sys.argv[2])
 print("id es: {}".format(id))
 
 status = 0
@@ -122,7 +192,7 @@ try:
     ic = Ice.initialize()
 
     adapter = ic.createObjectAdapterWithEndpoints("NodoAdapter", "default -p 1000{}".format(id))
-    object = NodoI(id, key)
+    object = NodoI(id)
     adapter.add(object, ic.stringToIdentity("Nodo_{}".format(id)))
     adapter.activate()
 
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Suma multiparte
+# Trillium multiparte
 
 Primero crear un ambiente virtual e instalar dependencias
 
@@ -13,20 +13,9 @@ pip install -r requirements.txt
 Se deben ejecutar 3 nodos en 3 terminales distintas utilizando el comando 
 
 ```
-python NodoI.py <id> <key>
+python NodoI.py <id>
 ```
 
 - **id** es el número que le corresponde al nodo, debe ser desde el 0 al 2.
-- **key** es el valor secreto del nodo
 
-
-
-Los nodos intentaran conectarse al valor 0 si no hay un valor tomaran ese id.
-Si ese nodo ya existe, intentará lo mismo con el siguiente y asi...
-
-Una vez tenga su id, intentará establecer conexión con todos los N nodos de la red.
-
-
-
-
-Aun existe el problema de saber con anticipación cuantos nodos hay en la red.
+Cada nodo, dependiendo del id se corresponderá con A, B o C.
