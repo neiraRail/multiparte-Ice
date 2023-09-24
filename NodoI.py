fdec2cdf9d1b51a93ba19ca1e2913893daba2eb7
Rail Neira <r.neira03@ufromail.cl>
2023-09-23 21:09:25 -0300

    Migra todos los prints a logging asignando niveles de logging y agregando la opción --debug para activarlos

--- NodoI.py
+++ NodoI.py
@@ -1,8 +1,6 @@
 import threading
 import Multiparte
-import random, asyncio
-
-n = 3
+import random, logging
 
 class NodoI(Multiparte.Nodo, threading.Thread):
     def __init__(self, id):
@@ -78,28 +76,28 @@ class NodoI(Multiparte.Nodo, threading.Thread):
             proxies = []
             for i in range(n):
                 if i != int(self.id): 
-                    print("Tengo que conectarme al nodo {}".format(i))
+                    logging.info("Tengo que conectarme al nodo {}".format(i))
                     base = ic.stringToProxy("Nodo_{}:default -p 1000{}".format(i,i))
                     conectado = False
                     while not conectado:
                         try:
-                            print("Intento")
+                            logging.debug("Intento")
                             proxy = Multiparte.NodoPrx.checkedCast(base)
                             if not proxy:
                                 raise RuntimeError("Invalid proxy")
                             proxies.append(proxy)
                             conectado = True
-                            print("Conectado al nodo {} :D".format(i))
+                            logging.info("Conectado al nodo {} :D".format(i))
                         except:
                             self.__threadCondition.wait(3)     
-            print("---------------Conexión lista--------------------\n\n")
+            logging.info("---------------Conexión lista--------------------\n\n")
             while True:
                 self.nroPartesEnviadas = 0
                 self.key = self.primerXor()
                 self.partes = self.separarKey()
-                print("Mi número secreto (s) es {}".format(self.key))
-                print("Las partes que voy a compartir son {}".format(self.partes))
-                print("Mi vector tiene largo: {}".format(len(self.vector)))
+                logging.debug("Mi número secreto (s) es {}".format(self.key))
+                logging.debug("Las partes que voy a compartir son {}".format(self.partes))
+                logging.debug("Mi vector tiene largo: {}".format(len(self.vector)))
 
 
                 # Pide todas las partes incluyendo la propia.
@@ -110,20 +108,20 @@ class NodoI(Multiparte.Nodo, threading.Thread):
                     numeroDelNodo = proxy.ice_toString().split(" ")[0].split("_")[1]
                     while not parteRecibida:
                         try:
-                            print("Necesito la parte de {}".format(numeroDelNodo), end=" ")
+                            logging.debug("Necesito la parte de {}".format(numeroDelNodo))
                             parte = proxy.getMyPart("")
                             self.otrasPartes.append(parte)
-                            print("que es {}".format(parte))
+                            logging.debug("que es {}".format(parte))
                         except:
                             self.__threadCondition.wait(1)
-                            print("")
+                            logging.debug("")
                         else:
                             parteRecibida = True
 
                 self.sumaTotal = None # Importante borrar para no enviar por accidente
                 self.sumaParcial = sum(self.otrasPartes)
-                print("Las partes que he recolectado son: {}".format(self.otrasPartes), end=" ")
-                print("Y suman {}".format(self.sumaParcial))
+                logging.debug("Las partes que he recolectado son: {}".format(self.otrasPartes))
+                logging.debug("Y suman {}".format(self.sumaParcial))
 
                 # Suma las partes de otros y genera su suma parcial
                 self.sumasParciales = []
@@ -135,19 +133,19 @@ class NodoI(Multiparte.Nodo, threading.Thread):
                     numeroDelNodo = proxy.ice_toString().split(" ")[0].split("_")[1]
                     while not sumaRecibida:
                         try:
-                            print("Necesito la suma parcial de {}".format(numeroDelNodo), end=" ")
+                            logging.debug("Necesito la suma parcial de {}".format(numeroDelNodo))
                             suma = proxy.getPartialSum()
                             self.sumasParciales.append(suma)
-                            print("que es {}".format(suma))
+                            logging.debug("que es {}".format(suma))
                         except:
                             self.__threadCondition.wait(1)
-                            print("")
+                            logging.debug("")
                         else:
                             sumaRecibida = True
                 self.partes = [] # Es importante borrar partes para que en la siguiente iteración no se envíe una parte anterior por accidente
                 self.sumaTotal = sum(self.sumasParciales)
-                print("Las sumas que he recolectado son: {}".format(self.sumasParciales))
-                print("La suma final es: {}".format(self.sumaTotal))
+                logging.debug("Las sumas que he recolectado son: {}".format(self.sumasParciales))
+                logging.debug("La suma final es: {}".format(self.sumaTotal))
                 
                 sumasTotales = []
                 for i, proxy in enumerate(proxies):
@@ -155,13 +153,13 @@ class NodoI(Multiparte.Nodo, threading.Thread):
                     numeroDelNodo = proxy.ice_toString().split(" ")[0].split("_")[1]
                     while not sumaTotalRecibida:
                         try:
-                            print("Necesito la suma final de {}".format(numeroDelNodo), end=" ")
+                            logging.debug("Necesito la suma final de {}".format(numeroDelNodo))
                             total = proxy.getFinalSum()
                             sumasTotales.append(total)
-                            print("que es {}".format(total))
+                            logging.debug("que es {}".format(total))
                         except:
                             self.__threadCondition.wait(1)
-                            print("")
+                            logging.debug("")
                         else:
                             sumaTotalRecibida = True
 
@@ -169,12 +167,13 @@ class NodoI(Multiparte.Nodo, threading.Thread):
                 assert all(x == sumasTotales[0] for x in sumasTotales) # Si una de las sumas totales no coincide se detiene la ejecución
                 
                 St = sum(self.sumasParciales) % 2
-                print("St es: {}".format(St))
+                logging.info("St es: {}".format(St))
                 self.moverVector(St, self.key)
 
-
-                input("Presiona Enter para continuar...")
-                #TODO: Como evitar que se llamen a los datos de la iteración pasada
+                if "--debug" in sys.argv:
+                    input("Presiona Enter para continuar...")
+                else:
+                    pass
             # ic.waitForShutdown()
 
 # CODIGO DEL SERVER ---------------------------------------------------------------------------------------------
@@ -182,10 +181,16 @@ class NodoI(Multiparte.Nodo, threading.Thread):
 
 import sys, traceback, Ice
 
+if "--debug" in sys.argv:
+    logging.basicConfig(level=logging.DEBUG)
+else:
+    logging.basicConfig(level=logging.INFO)
+
 
 id = int(sys.argv[1])
-print("id es: {}".format(id))
+logging.debug("id es: {}".format(id))
 
+n = 3
 status = 0
 ic = None
 try:
@@ -201,7 +206,7 @@ try:
     ic.waitForShutdown()
 
 except:
-    traceback.print_exc()
+    traceback.logging.debug_exc()
     status = 1
 
 if ic:
@@ -209,7 +214,7 @@ if ic:
     try:
         ic.destroy()
     except:
-        traceback.print_exc()
+        traceback.logging.debug_exc()
         status = 1
 
 sys.exit(status)
--- README.md
+++ README.md
@@ -13,9 +13,11 @@ pip install -r requirements.txt
 Se deben ejecutar 3 nodos en 3 terminales distintas utilizando el comando 
 
 ```
-python NodoI.py <id>
+python NodoI.py <id> [--debug]
 ```
 
 - **id** es el número que le corresponde al nodo, debe ser desde el 0 al 2.
+
+- **--debug** es una flag opcional para imprimir en terminal todos los detalles de los estados internos de cada nodo: partes, sumas parciales, suma total, intentos de solicitud e **importante**, requerirá presionar Enter para avanzar en cada iteración.
 
 Cada nodo, dependiendo del id se corresponderá con A, B o C.
